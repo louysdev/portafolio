c0eeddaeffe6074c505df7881df0369f5e418d3a
louysdev <louysdev@gmail.com>
2024-02-25 16:58:51 -0400

    Deploy a github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://louysdev.github.io",
+  base: "/portafolio",
+  integrations: [tailwind()],
 });
